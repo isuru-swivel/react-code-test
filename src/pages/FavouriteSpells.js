670795a41d6614ee7cdfea34e9e7d9b7eb1c200c
Isuru Madusanka <isurum@swivelgroup.com.au>
2022-07-13 19:50:07 +0530

    added back button to favourite spells page

--- src/pages/FavouriteSpells.js
+++ src/pages/FavouriteSpells.js
@@ -1,13 +1,20 @@
 import React from "react";
 import SpellCard from "../components/SpellCard";
 import {useStateValue} from "../context/stateProvider";
+import {Link} from "react-router-dom";
+import {BsFillArrowLeftCircleFill} from "react-icons/bs";
 
 const FavouriteSpells = () => {
     const [{favouriteSpells}, dispatch] = useStateValue();
 
     return (
         <div className="container">
-            <h4 className="my-3 text-center">Favourite Spells</h4>
+            <div className="my-3">
+                <Link to={"/"}>
+                    <BsFillArrowLeftCircleFill size={30}/>
+                </Link>
+                <h4 className="text-center">Favourite Spells</h4>
+            </div>
             {
                 favouriteSpells.length > 0 ?
                     (favouriteSpells.map(spell => <SpellCard key={spell.index} spell={spell} isFavourite={true}/>))
